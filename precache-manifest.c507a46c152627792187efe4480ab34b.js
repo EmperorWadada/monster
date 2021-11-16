e16e1b72d0c9ba23210475dca28fcb762f5ccb81
Abdulshakur <emperor.cruzy@gmail.com>
2021-11-16 13:29:58 +0100

    Updates

---
+++ precache-manifest.c507a46c152627792187efe4480ab34b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b35b885e0527c72adec6adf42b233b7",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "7182b9f112e6774bc0a8",
+    "url": "/monster/static/css/main.4057b84b.chunk.css"
+  },
+  {
+    "revision": "9265742814159994725d",
+    "url": "/monster/static/js/2.3c5292f0.chunk.js"
+  },
+  {
+    "revision": "7db361069cc52ba48096",
+    "url": "/monster/static/js/3.63a8b96a.chunk.js"
+  },
+  {
+    "revision": "7182b9f112e6774bc0a8",
+    "url": "/monster/static/js/main.7998995a.chunk.js"
+  },
+  {
+    "revision": "d6c7ebb72b5370636321",
+    "url": "/monster/static/js/runtime~main.8f10258f.js"
+  }
+]);
