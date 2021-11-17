4397b481ad715ae6d0faaf55906fc11a48b567c9
Abdulshakur <emperor.cruzy@gmail.com>
2021-11-17 09:42:36 +0100

    little e-comerce prject on reactjs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import React, { Component } from 'react'
-import {CardList} from './components/card-list/card-list.component'
-import Search from './components/card-list/search-box/search-box'
+import React from 'react'
 import './App.css';
+import Homepage from './homepage/homepage.component'
 
-class App extends Component {
-
-  constructor(){
-    super();
-
-    this.state = {
-      monster: [],
-      searchFiled: ''
-
-    };
-  }
-
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(data => data.json())
-    .then(users => this.setState({monster: users}))
-    
-  }
-
-  render(){
-    const {searchFiled, monster} = this.state;
-    const filterMonsters = monster.filter(monster => 
-      monster.name.toLowerCase().includes(searchFiled.toLowerCase()))
-    return(
-      
-      <div className='App'>
-        <h1>{this.state.searchFiled}</h1>
-        <Search placeholder="Search Monster" handleChange={(e) => this.setState({searchFiled: e.target.value})}/>
-        <CardList monster={filterMonsters} />
-      </div>
-    )
-  }
+function App() {
+  return(
+    <div>
+      <Homepage />
+    </div>
+  )
 }
 
 export default App;
- 
+
